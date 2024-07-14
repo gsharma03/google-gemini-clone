599e25a0b1f1e44ac2ef6971926a2964163fb5a1
gsharma03 <gauravsh.9315@gmail.com>
2024-07-14 23:26:07 +0530

    gemini clone created successfully

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main/Main"
+import Sidebar from "./components/Sidebar/sidebar"
+
+const App = () => {
+  return (
+    <>
+    <Sidebar />
+    <Main />
+    </>
+  )
+}
+
+export default App
